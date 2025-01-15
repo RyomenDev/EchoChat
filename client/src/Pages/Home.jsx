67fbaa380a5a190b894a8ee11a6f871dc3bb024f
RyomenDev <aakashvatsh007@gmail.com>
2025-01-15 09:41:47 +0530

    design: HomePage

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,3 +1,4 @@
+import "./styles.css";
 import { useState } from "react";
 import conf from "../conf/conf";
 
@@ -8,7 +9,7 @@ const providersNames = ["google", "github"];
 
 const LoginButton = (props) => (
   <a href={`${backendUrl}/api/connect/${props.providerName}`}>
-    <button className="bg-blue-500 text-white py-2 px-6 rounded-md shadow-md hover:bg-blue-600 focus:outline-none">
+    <button className="bg-[#0f0] opacity-100 text-white py-2 px-6 rounded-md hover:bg-[#0f5]">
       Connect to {props.providerName}
     </button>
   </a>
@@ -58,19 +59,35 @@ const Home = (props) => {
   }
 
   return (
-    <div className="min-h-screen flex flex-col justify-center items-center bg-gray-100 p-6">
-      <div className="bg-white p-6 rounded-xl shadow-lg max-w-md w-full text-center">
-        <h1 className="text-3xl font-semibold text-gray-800 mb-4">
-          Welcome to Strapi ChatRoom
-        </h1>
-        <p className="text-lg text-gray-600 mb-6">
-          Join the conversation, meet new people, and make connections in one
-          shared room.
-        </p>
-        <p className="text-lg font-medium text-gray-700 mb-6">{text}</p>
-        {buttons}
-      </div>
-    </div>
+    <>
+      <section className="">
+        <div className=""></div>
+        {Array.from({ length: 200 }).map((_, index) => (
+          <span key={index} className=""></span>
+        ))}
+
+        <div className="signin">
+          <div className="content">
+            <h2 className=" text-center "> Welcome to Strapi ChatRoom</h2>
+            <p className=" text-center text-xl text-[#0f0]">{text}</p>
+            {buttons}
+          </div>
+        </div>
+      </section>
+      {/* <div className="min-h-screen flex flex-col justify-center items-center bg-gray-100 p-6">
+        <div className="bg-white p-6 rounded-xl shadow-lg max-w-md w-full text-center">
+          <h1 className="text-3xl font-semibold text-gray-800 mb-4">
+            Welcome to Strapi ChatRoom
+          </h1>
+          <p className="text-lg text-gray-600 mb-6">
+            Join the conversation, meet new people, and make connections in one
+            shared room.
+          </p>
+          <p className="text-lg font-medium text-gray-700 mb-6">{text}</p>
+          {buttons}
+        </div>
+      </div> */}
+    </>
   );
 };
 
